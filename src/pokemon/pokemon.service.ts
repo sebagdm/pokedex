4526986cc33b02251f002fcede35b4e545267ef3
Juan Sebastián Contini <continisebastian@gmail.com>
2023-06-07 02:26:08 -0300

    add feature conect to database MongoDB

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  constructor(@InjectModel(Pokemon.name)
+  private readonly pokemonModel: Model<Pokemon>,) {
+
+  }
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase()
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto)
+      return pokemon
+    } catch (error) {
+      this.handleException(error)
+    }
+
   }
 
   findAll() {
@@ -22,5 +36,15 @@ export class PokemonService {
 
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
+  }
+
+  private handleException(error: any) {
+    const { code, keyValue } = error
+    if (code === 11000) {
+      throw new BadRequestException(`Pokemon exsist in db ${JSON.stringify(keyValue)}`)
+    }
+    console.log(error)
+    throw new InternalServerErrorException(`can't update pokemon - check server log`)
+
   }
 }
